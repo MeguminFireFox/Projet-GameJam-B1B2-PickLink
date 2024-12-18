39ecf3606215de5eb90f615717d14e9b766bc0e2
LordEagleEye <geoffrey.voineau@e-artsup.net>
2024-12-18 14:38:04 +0100

    Eau_Réparer

--- PickLink/Assets/_VFXpack/Scripts/Editor/LightAnimation_Editor.cs
+++ PickLink/Assets/_VFXpack/Scripts/Editor/LightAnimation_Editor.cs
@@ -11,32 +11,7 @@ using UnityEngine.Rendering;
 namespace SimpleVFXs
 {
 
-#if UNITY_EDITOR
 
-    [CustomEditor(typeof(LightAnimation))]
-    class LightAnimation_Editor : Editor
-    {
-        public override void OnInspectorGUI()
-        {
-            LightAnimation myScript = (LightAnimation)target;
-
-            DrawDefaultInspector();
-
-            GUILayout.Space(10);
-            if (GUILayout.Button("Play"))
-            {
-                myScript.Play();
-            }
-
-            if (GUILayout.Button("Play in reverse"))
-            {
-                myScript.PlayInReverse();
-            }
-
-        }
-    }
-
-#endif
 
 
 }
