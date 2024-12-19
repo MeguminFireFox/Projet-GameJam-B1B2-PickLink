0cac7cf6d9a91d179881eb5625e2f451c045cc7b
MeguminFireFox <ambrehardy013@gmail.com>
2024-12-19 14:22:43 +0100

    les anim du perso principal

--- PickLink/Assets/Script/Player/Score.cs
+++ PickLink/Assets/Script/Player/Score.cs
@@ -13,6 +13,8 @@ public class Score : MonoBehaviour
     [SerializeField] public int CoinCount { get; set; }
     private bool _quota = false;
     private bool _canQuota = false;
+    [SerializeField] private Animator _animator;
+    [SerializeField] private List<GameObject> _listAccesoire;
 
     IEnumerator ActiveQuota()
     {
@@ -22,18 +24,25 @@ public class Score : MonoBehaviour
             case "Picsou":
                 Quota = ListQuota[0];
                 _role.Objectif = "Ramasser le plus de Pieces";
+                _listAccesoire[2].SetActive(true);
+                _listAccesoire[3].SetActive(true);
                 break;
             case "Killer":
                 Quota = ListQuota[1];
                 _role.Objectif = "Tuer le plus d'Ennemis";
+                _listAccesoire[5].SetActive(true);
                 break;
             case "Voltigeur":
                 Quota = ListQuota[2];
                 _role.Objectif = "Sauter le plus de fois";
+                _listAccesoire[0].SetActive(true);
+                _listAccesoire[3].SetActive(true);
                 break;
             case "Glouton":
                 Quota = ListQuota[3];
                 _role.Objectif = "Ramasser le plus de Viande";
+                _listAccesoire[1].SetActive(true);
+                _listAccesoire[4].SetActive(true);
                 break;
         }
         yield return new WaitForSeconds(0.1f);
@@ -83,6 +92,7 @@ public class Score : MonoBehaviour
             if (_invincibility) return;
 
             TeamLife.Instance.Life -= 1;
+            _animator.SetBool("IsAie", true);
             StartCoroutine(Wait());
         }
     }
@@ -90,7 +100,9 @@ public class Score : MonoBehaviour
     IEnumerator Wait()
     {
         _invincibility = true;
-        yield return new WaitForSeconds(3);
+        yield return new WaitForSeconds(0.1f);
+        _animator.SetBool("IsAie", false);
+        yield return new WaitForSeconds(2.9f);
         _invincibility = false;
     }
 }
